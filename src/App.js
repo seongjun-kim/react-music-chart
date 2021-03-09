79d62d993511d401eb47f320896995a4c7c15ddd
seongjun_kim <seongjun_kim@tmax.co.kr>
2021-03-09 23:31:32 +0900

    feat: music chart api 연동 (순위/제목/사진 표시)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MusicChart from "./MusicChart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    this._getMusicChart();
+  }
+
+  _renderMusicChart = () => {
+    const musicChart = this.state.musicChart.map((data, index) => {
+      return <MusicChart rank={index + 1} musicData={data} />;
+    });
+    return musicChart;
+  };
+
+  _getMusicChart = async () => {
+    const musicChart = await this._callApi();
+    this.setState({
+      musicChart,
+    });
+  };
+
+  _callApi = () => {
+    return fetch(`https://itunes.apple.com/us/rss/topalbums/limit=100/json`)
+      .then((res) => {
+        try {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error(res);
+          }
+        } catch (err) {
+          console.log("error!:", err.message);
+          // return WHATEVER_YOU_WANT_TO_RETURN;
+        }
+      })
+      .then((resJson) => {
+        return resJson.feed.entry;
+      })
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    const { musicChart } = this.state;
+    return (
+      <div className={musicChart ? "App" : "App-loading"}>
+        {musicChart ? this._renderMusicChart() : "Loading ..."}
+      </div>
+    );
+  }
 }
 
 export default App;
